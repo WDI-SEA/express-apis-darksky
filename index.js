76b7a6f1739413e692c0343944163663c912c440
mtom92 <torresmoranm@gmail.com>
2019-04-11 11:17:06 -0700

    working

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+require('dotenv').config()
 // Require node modules that you need
+var geocoder = require('simple-geocoder')
 var express = require('express');
 var layouts = require('express-ejs-layouts');
 var parser = require('body-parser');
+var request = require('request')
+var moment = require('moment')
+
 
 // Declare your app
 var app = express();
@@ -20,7 +25,58 @@ app.get('/', function(req, res){
 });
 
 app.post('/', function(req, res){
-  res.render('result');
+  var urlToCall = process.env.DARK_SKY_BASE_URL
+  var locationx
+  var locationy
+  var temp
+  var todaySum
+  var daily
+  var days = []
+
+  geocoder.geocode(req.body.location, function(success, locations) {
+	if(success) {
+     locationx = locations.y
+		 locationy = locations.x
+     urlToCall = urlToCall + locationx + ','+ locationy
+
+     request(urlToCall, function(error, response, body) {
+       // Parse the data
+       var result = JSON.parse(body);
+
+       // Look at the data
+
+       // TODO: Do something with that data!
+       temp = result.currently.temperature
+       todaySum = result.currently.summary
+       daily = result.daily.data
+       daily.forEach((day)=>{
+         days.push(moment.unix(day.time).format('dddd'))
+       })
+
+       for (var i = 0; i < daily.length; i++) {
+         daily[i].date = days[i]
+       }
+
+
+       res.render('result',{
+         daily:daily,
+         todaySum:todaySum,
+         temp : temp,
+         city:req.body.location,
+         locationx: locationx.toFixed(6),
+         locationy: locationy.toFixed(6)
+       });
+
+   });
+	}else{
+    console.log("something went wrong");
+  }
+
+
+
+});
+
+
 });
 
 // Listen on PORT 3000
