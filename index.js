de4ac0ea45c7880f9a8fa6878ae69055e358f0ca
klar2d2 <koberyan@comcast.net>
2019-08-15 09:05:22 -0700

    initial

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 var express = require('express');
 var layouts = require('express-ejs-layouts');
 var parser = require('body-parser');
+var geocoder = require('simple-geocoder')
+var request = require('request')
+require('dotenv').config()
 
 // Declare your app
 var app = express();
-
 // Tell express what view engine you want to use
 app.set('view engine', 'ejs');
 
@@ -19,8 +21,26 @@ app.get('/', function(req, res){
   res.render('home');
 });
 
-app.post('/', function(req, res){
-  res.render('result');
+app.post('/result', function(req, res){
+  // see if we get the  Data from the form
+  var local = req.body.city
+  geocoder.geocode(req.body.city, function(success, locations) {
+  	if(success) {
+      console.log('location:', locations.y, locations.x)
+      }
+      var weather = process.env.DARK_SKY_BASE_URL + locations.y + ',' + locations.x;
+      console.log(weather)
+      request(weather, (error, response, body) => {
+        var results = JSON.parse(body)
+        console.log(results)
+        res.render('result', {
+          results: JSON.parse(body),
+          local: local,
+          locationsY: locations.y,
+          locationsX: locations.x
+        });
+    });
+      })
 });
 
 // Listen on PORT 3000
