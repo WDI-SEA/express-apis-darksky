de1b20f22265ea5ec3205ecbe93e6e1b894f995d
madmonkeyaudio-js <jonathangross@Jonathans-MacBook-Pro.local>
2019-08-15 08:45:51 -0700

    hw commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // Require node modules that you need
-var express = require('express');
-var layouts = require('express-ejs-layouts');
-var parser = require('body-parser');
-
+const express = require('express');
+const layouts = require('express-ejs-layouts');
+const parser = require('body-parser');
+const request = require('request');
+const geocoder = require('simple-geocoder');
+const moment = require('moment');
+require('dotenv').config();
 // Declare your app
-var app = express();
+const app = express();
 
 // Tell express what view engine you want to use
 app.set('view engine', 'ejs');
@@ -14,13 +17,43 @@ app.use(layouts);
 app.use(express.static('static'));
 app.use(parser.urlencoded({ extended: false }));
 
+
 // Declare routes
 app.get('/', function(req, res){
   res.render('home');
 });
 
-app.post('/', function(req, res){
+app.get('/result', function(req, res){
   res.render('result');
+})
+
+app.post('/result', function(req, res){
+  geocoder.geocode(req.body.name, function(success, locations) {
+    if(success) {
+
+      let urlToCall = process.env.DARK_SKY_BASE_URL + locations.y + ',' + locations.x;
+      //console.log(urlToCall);
+      request(urlToCall, (err, response, body) => {
+        if(err || response.statusCode != 200){
+          res.send('404');
+        } else {
+        let result = JSON.parse(body);
+        let dailyArr = [];
+        for (i = 0; i < 5; i++){
+          dailyArr.push(result.daily.data[i]);
+        }
+        res.render('result', {
+
+          name: req.body.name,
+          summary: result.daily.summary,
+          days: dailyArr
+          
+        })
+        }
+      })
+    
+    }
+  })
 });
 
 // Listen on PORT 3000
