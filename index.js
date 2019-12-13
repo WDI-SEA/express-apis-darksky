9cdb5423bb503c512d7d7e551c654f173c2571d1
kennansalisbury <kennansalisbury@gmail.com>
2019-12-12 18:42:24 -0800

    added api request

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Require node modules that you need
+require('dotenv').config()
 let express = require('express');
 let layouts = require('express-ejs-layouts');
 let parser = require('body-parser');
 let geocoder = require('simple-geocoder')
+let request = require('request')
 
 // Declare your app
 let app = express();
@@ -24,6 +26,12 @@ app.post('/', function(req, res){
   //geocode
   geocoder.geocode(req.body.location, function(success, locations) {
     if(success) {
+      request(process.env.DARK_SKY_API_BASE_URL + locations.y + ',' + locations.x, (error, response, body) => {
+        console.log('error:', error)
+        console.log('statusCode:', response && response.statusCode)
+        let result = JSON.parse(body)
+        console.log(result)
+      })
       res.render('result', {myLocation: req.body.location, myCoordinates: locations})
     }
   });
