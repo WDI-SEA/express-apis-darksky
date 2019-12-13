28bcd7addcb24e4339adc03a037da463482fb66f
dschawel <david.schawel@gmail.com>
2019-12-12 22:30:29 -0800

    got API to work

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Require node modules that you need
-var express = require('express');
-var layouts = require('express-ejs-layouts');
-var parser = require('body-parser');
-var geocoder = require('simple-geocoder');
-var urlToCall = process.env.DARK_SKY_BASE_URL + lat + ',' + lng;
+// require(dotenv).config()
+let express = require('express');
+let layouts = require('express-ejs-layouts');
+let parser = require('body-parser');
+let geocoder = require('simple-geocoder');
+let request = require('request')
+
 
 // Declare your app
 var app = express();
@@ -18,27 +20,31 @@ app.use(parser.urlencoded({ extended: false }));
 
 // Declare routes
 app.get('/', function(req, res){
-  res.render('home');
-    request('https://api.darksky.net/forecast/' + urlToCall, function (error, response, body) {
-        console.log('0')
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
-        res.send(body)
-        var result = JSON.parse(body);
-        console.log(result);
-    })
+  res.render('home') 
 });
 
 app.post('/results', function(req, res){
   let location = req.body.location
-  res.render('result', { myData: location });
-  console.log(req.body);
-  geocoder.geocode(req.body.location, function(success, locations) {
+  geocoder.geocode(location, (success, locations) => {
     if(success) {
-      console.log("Location: ", locations.x, locations.y);
+      console.log("Location: ", locations.x, locations.y)
+      let coord = [locations.x.toFixed(2), locations.y.toFixed(2)]
+      res.render('result', {myData: location, myCoord: coord});
+      let lng = locations.x.toFixed(2)
+      let lat = locations.y.toFixed()
+      console.log(lat)
+      console.log(lng)
+      request('https://api.darksky.net/forecast/22e75dafa4c9846887b39a60718a76f8/'+lat+','+lng, (error, response, body) => {
+        console.log('0')
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      // console.log('body:', body); // Print the HTML for the Google homepage.
+      let result = JSON.parse(body)
+      console.log(result)
+    })
     }
-  });
+  })
+  console.log(req.body)
 });
 
 // Listen on PORT 3000
